b77576a94c1fd04e748b27ed813ab6f79cc5be3f
Rocco Howard <rocco@hnh.digital>
2019-08-13 04:25:40 +0000

    Updated dependencies

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -19,7 +19,7 @@ class ModelTest extends TestCase
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         $this->configureDatabase();
     }
